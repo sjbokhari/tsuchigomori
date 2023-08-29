86c984004177c1ce91ee55b987c0baf2a7108cb2
sjbokhari0 <60392358+sjbokhari0@users.noreply.github.com>
2023-08-29 16:25:33 +0200

    added basic function

--- src/go.mod
+++ src/go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/antchfx/xpath v1.1.10 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly/v2 v2.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
@@ -21,5 +20,5 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -7,20 +7,30 @@ import (
 )
 
 func main() {
-	c := colly.NewCollector()
-
-	colly.AllowedDomains("www.gelbeseiten.de", "gelbeseiten.de")
-	colly.MaxDepth(3)
-	colly.Async(true)
-
-	// Find and visit all links
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: gelbeseiten.de
+		//colly.AllowedDomains("gelbeseiten.de"),
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+	)
+	//*[@id="treffer_8000162004626839"]/a/h2
+	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		//c.Visit(e.Request.AbsoluteURL(link))
 	})
 
+	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://www.gelbeseiten.de")
+	// Start scraping on https://www.gelbeseiten.de
+	what := "krankenhaus"
+	city := "frankfurt"
+	c.Visit(fmt.Sprintf("https://www.gelbeseiten.de/suche/%s/%s?umkreis=4000", what, city))
 }
